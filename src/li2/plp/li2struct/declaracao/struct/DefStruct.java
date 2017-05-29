bdb1b2b70f82ac522a7d2693b9fcc07423f9aee1
karinegmg <kgmg@cin.ufpe.br>
2017-05-29 10:49:29 -0300

    modificando as classes .../declaracao/struct (agora implementam a interface Declaracao de li2struct)

--- src/li2/plp/li2struct/declaracao/struct/DefStruct.java
+++ src/li2/plp/li2struct/declaracao/struct/DefStruct.java
@@ -2,29 +2,47 @@ package li2.plp.li2struct.declaracao.struct;
 
 import li2.plp.expressions2.memory.IdentificadorJaDeclaradoException;
 import li2.plp.expressions2.memory.IdentificadorNaoDeclaradoException;
-import li2.plp.imperative1.declaration.Declaracao;
+import li2.plp.expressions2.memory.VariavelJaDeclaradaException;
+import li2.plp.expressions2.memory.VariavelNaoDeclaradaException;
 import li2.plp.imperative1.memory.AmbienteCompilacaoImperativa;
 import li2.plp.imperative1.memory.AmbienteExecucaoImperativa;
 import li2.plp.imperative1.memory.EntradaVaziaException;
+import li2.plp.imperative2.memory.ProcedimentoJaDeclaradoException;
+import li2.plp.imperative2.memory.ProcedimentoNaoDeclaradoException;
+import li2.plp.li2struct.declaracao.Declaracao;
+import li2.plp.li2struct.exception.InstanciaStructJaDeclaradaException;
+import li2.plp.li2struct.exception.InstanciaStructNaoDeclaradaException;
+import li2.plp.li2struct.exception.StructJaDeclaradaException;
+import li2.plp.li2struct.exception.StructNaoDeclaradaException;
 import li2.plp.li2struct.expression.leftExpression.Id;
+import li2.plp.li2struct.memory.AmbienteCompilacaoli2Struct;
+import li2.plp.li2struct.memory.AmbienteExecucaoli2Struct;
 
-public class DefStruct extends Declaracao{
+public class DefStruct implements Declaracao{
 
+	private Id id;
+	private DeclaracaoChave chave;
+	private ListaDecAtributoStruct listaDecAtributoStruct;
 	
-	public DefStruct(Id id, DeclaracaoChave chave, ListaDecAtributoStruct listadecattstruct) {
-		// TODO Auto-generated constructor stub
+	public DefStruct(Id id, DeclaracaoChave chave, ListaDecAtributoStruct listaDecAtributoStruct) {
+		this.id = id;
+		this.chave = chave;
+		this.listaDecAtributoStruct = listaDecAtributoStruct;
 	}
 
 	@Override
-	public AmbienteExecucaoImperativa elabora(AmbienteExecucaoImperativa ambiente)
-			throws IdentificadorJaDeclaradoException, IdentificadorNaoDeclaradoException, EntradaVaziaException {
+	public AmbienteExecucaoli2Struct elabora(AmbienteExecucaoli2Struct ambiente)
+			throws VariavelJaDeclaradaException, VariavelNaoDeclaradaException, ProcedimentoJaDeclaradoException,
+			ProcedimentoNaoDeclaradoException, StructJaDeclaradaException, StructNaoDeclaradaException,
+			InstanciaStructNaoDeclaradaException, InstanciaStructJaDeclaradaException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean checaTipo(AmbienteCompilacaoImperativa ambiente)
-			throws IdentificadorJaDeclaradoException, IdentificadorNaoDeclaradoException, EntradaVaziaException {
+	public boolean checaTipo(AmbienteCompilacaoli2Struct ambiente)
+			throws VariavelJaDeclaradaException, VariavelNaoDeclaradaException, ProcedimentoJaDeclaradoException,
+			ProcedimentoNaoDeclaradoException, StructJaDeclaradaException, StructNaoDeclaradaException {
 		// TODO Auto-generated method stub
 		return false;
 	}
--- src/li2/plp/li2struct/declaracao/struct/InstanciaValoresStruct.java
+++ src/li2/plp/li2struct/declaracao/struct/InstanciaValoresStruct.java
@@ -1,24 +1,32 @@
 package li2.plp.li2struct.declaracao.struct;
 
-import li2.plp.expressions2.memory.IdentificadorJaDeclaradoException;
-import li2.plp.expressions2.memory.IdentificadorNaoDeclaradoException;
-import li2.plp.imperative1.declaration.Declaracao;
-import li2.plp.imperative1.memory.AmbienteCompilacaoImperativa;
-import li2.plp.imperative1.memory.AmbienteExecucaoImperativa;
-import li2.plp.imperative1.memory.EntradaVaziaException;
+import li2.plp.expressions2.memory.VariavelJaDeclaradaException;
+import li2.plp.expressions2.memory.VariavelNaoDeclaradaException;
+import li2.plp.imperative2.memory.ProcedimentoJaDeclaradoException;
+import li2.plp.imperative2.memory.ProcedimentoNaoDeclaradoException;
+import li2.plp.li2struct.declaracao.Declaracao;
+import li2.plp.li2struct.exception.InstanciaStructJaDeclaradaException;
+import li2.plp.li2struct.exception.InstanciaStructNaoDeclaradaException;
+import li2.plp.li2struct.exception.StructJaDeclaradaException;
+import li2.plp.li2struct.exception.StructNaoDeclaradaException;
+import li2.plp.li2struct.memory.AmbienteCompilacaoli2Struct;
+import li2.plp.li2struct.memory.AmbienteExecucaoli2Struct;
 
-public class InstanciaValoresStruct extends Declaracao{
+public class InstanciaValoresStruct implements Declaracao{
 
 	@Override
-	public AmbienteExecucaoImperativa elabora(AmbienteExecucaoImperativa ambiente)
-			throws IdentificadorJaDeclaradoException, IdentificadorNaoDeclaradoException, EntradaVaziaException {
+	public AmbienteExecucaoli2Struct elabora(AmbienteExecucaoli2Struct ambiente)
+			throws VariavelJaDeclaradaException, VariavelNaoDeclaradaException, ProcedimentoJaDeclaradoException,
+			ProcedimentoNaoDeclaradoException, StructJaDeclaradaException, StructNaoDeclaradaException,
+			InstanciaStructNaoDeclaradaException, InstanciaStructJaDeclaradaException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean checaTipo(AmbienteCompilacaoImperativa ambiente)
-			throws IdentificadorJaDeclaradoException, IdentificadorNaoDeclaradoException, EntradaVaziaException {
+	public boolean checaTipo(AmbienteCompilacaoli2Struct ambiente)
+			throws VariavelJaDeclaradaException, VariavelNaoDeclaradaException, ProcedimentoJaDeclaradoException,
+			ProcedimentoNaoDeclaradoException, StructJaDeclaradaException, StructNaoDeclaradaException {
 		// TODO Auto-generated method stub
 		return false;
 	}
